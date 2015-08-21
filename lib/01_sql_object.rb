5b6699cdeb24bb594b27da5c93fb8a582f598a2c
Ronald Joo <rjoo189@gmail.com>
2015-08-20 20:39:07 -0400

    halfway through creating a sql object

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -5,18 +5,39 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
+    return @columns if @columns
+    cols = DBConnection.execute2(<<-SQL).first
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      LIMIT
+        0
+    SQL
+    cols.map!(&:to_sym)
+    @columns = cols
     # ...
   end
 
   def self.finalize!
+    self.columns.each do |name|
+      define_method(name) do
+        self.attributes[name]
+      end
+
+      define_method("#{name}=") do |value|
+        self.attributes[name] = value
+      end
+
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name || self.name.underscore.pluralize
   end
 
   def self.all
@@ -32,10 +53,20 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |key, val|
+      attr_name = key.to_sym
+      if self.class.columns.include?(attr_name)
+        self.send("#{attr_name}=", val)
+        # self.attributes[attr_name] = val
+      else
+        raise "unknown attribute '#{attr_name}'"
+      end
+    end
+
   end
 
   def attributes
+    @attributes ||= {}
     # ...
   end
 
